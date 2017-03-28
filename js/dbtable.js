359c5476d137b390ef525d0c05dbe3c4f9643b86
belizwp <belizwp@live.com>
2017-03-29 00:04:12 +0700

    Fixed checkbox

--- js/dbtable.js
+++ js/dbtable.js
@@ -1,16 +1,57 @@
 $(document).ready(function(){
-$("#mytable #checkall").click(function () {
-        if ($("#mytable #checkall").is(':checked')) {
-            $("#mytable input[type=checkbox]").each(function () {
+    $("#announce-table #checkall").click(function () {
+        if ($("#announce-table #checkall").is(':checked')) {
+            $("#announce-table input[type=checkbox]").each(function () {
                 $(this).prop("checked", true);
             });
 
         } else {
-            $("#mytable input[type=checkbox]").each(function () {
+            $("#announce-table input[type=checkbox]").each(function () {
+                $(this).prop("checked", false);
+            });
+        }
+    });
+
+    $("#contact-table #checkall").click(function () {
+        if ($("#contact-table #checkall").is(':checked')) {
+            $("#contact-table input[type=checkbox]").each(function () {
+                $(this).prop("checked", true);
+            });
+
+        } else {
+            $("#contact-table input[type=checkbox]").each(function () {
+                $(this).prop("checked", false);
+            });
+        }
+    });
+
+    $("#news-table #checkall").click(function () {
+        if ($("#news-table #checkall").is(':checked')) {
+            $("#news-table input[type=checkbox]").each(function () {
+                $(this).prop("checked", true);
+            });
+
+        } else {
+            $("#news-table input[type=checkbox]").each(function () {
+                $(this).prop("checked", false);
+            });
+        }
+    });
+
+    $("#member-table #checkall").click(function () {
+        if ($("#member-table #checkall").is(':checked')) {
+            $("#member-table input[type=checkbox]").each(function () {
+                $(this).prop("checked", true);
+            });
+
+        } else {
+            $("#member-table input[type=checkbox]").each(function () {
                 $(this).prop("checked", false);
             });
         }
     });
     
     $("[data-toggle=tooltip]").tooltip();
+
 });
+
